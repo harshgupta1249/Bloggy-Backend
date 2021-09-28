49a8f419e8865697daa5b94b5a7221cb6cb1337f
Harsh <harshgupta1249@gmail.com>
2021-09-28 17:37:22 +0530

    spelling bug fixed

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../models/user');
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
 //UPDATE
@@ -12,7 +12,8 @@ router.put('/:id', async (req, res) => {
         try{
             const updatedUser = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
-            });
+            },
+            {new:true});
             res.status(200).json(updatedUser);
         }
         catch(err){
