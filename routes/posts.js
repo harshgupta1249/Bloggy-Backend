cad479cdae6838210f776c2f23a997587e00f240
Harsh <harshgupta1249@gmail.com>
2021-11-14 16:28:56 +0530

    bug fixed in delete post route

--- routes/posts.js
+++ routes/posts.js
@@ -45,20 +45,18 @@ router.put('/update/:id', async (req, res)=>{
 });
 
 //DELETE POST
-router.put('/delete/:id', async (req, res)=>{
+router.delete('/del/:id', async (req, res)=>{
     try{
         const post = await Post.findById(req.params.id);
 
         if(post.username === req.body.username){
-
             try{
-                await Post.delete();
+                await post.delete();
                 res.status(200).json("Post has been deleted");
             }
             catch(err){
                 res.status(500).json(err);
             }
-
         }
         else{
             res.status(401).json("Not authorized to delete");
