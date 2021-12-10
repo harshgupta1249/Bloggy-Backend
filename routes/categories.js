0775917fac92b7afeebfe4174a3263327ac64343
Harsh <harshgupta1249@gmail.com>
2021-12-10 10:15:38 +0530

    updated category route

--- routes/categories.js
+++ routes/categories.js
@@ -3,7 +3,7 @@ const Category = require('../models/Category');
 
 router.post('/', async (req, res)=>{
 
-    const newCat = new Category(req.body);
+    const newCat = new Category({name: req.body.name});
     try{
         const savedCat = await newCat.save();
         res.status(200).json(savedCat);
